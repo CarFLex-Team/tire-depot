82083205801c5a87ec78cdf8c088f5db8fcd5919
Mazen Essam <mazenessam2011@outlook.com>
2026-06-10 00:23:30 +0300

    remove module in reset pass

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,10 +1,8 @@
 import { Suspense } from "react";
-import ResetPassword from "../../../components/ClientRender/ResetPassword";
+// import ResetPassword from "../../../components/ClientRender/ResetPassword";
 
 export default function ResetPasswordPage() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <ResetPassword />
-    </Suspense>
+    <Suspense fallback={<p>Loading...</p>}>{/* <ResetPassword /> */}</Suspense>
   );
 }
