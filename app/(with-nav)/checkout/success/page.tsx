101df323463dbcfab082be0dd8ca3ad82d259b42
Mazen Essam <mazenessam2011@outlook.com>
2026-08-17 19:54:32 +0300

    Add suspense for search param in success checkout

--- app/(with-nav)/checkout/success/page.tsx
+++ app/(with-nav)/checkout/success/page.tsx
@@ -1,9 +1,23 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import CheckoutSuccessClient from "@/components/CheckoutSuccessClient";
+import AnimatedLogo from "@/components/AnimatedLogo";
+import { Suspense } from "react";
 
 export default async function CheckoutSuccessPage() {
   // const { total, subtotal, tax } = JSON.parse(cookie.value);
   //   cookieStore.delete("checkout_session"); // ← clean up
-  return <CheckoutSuccessClient />;
+  return (
+    <main className="bg-brand-dark min-h-screen">
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <AnimatedLogo withText={false} width={20} height={20} />
+          </div>
+        }
+      >
+        <CheckoutSuccessClient />;
+      </Suspense>
+    </main>
+  );
 }
